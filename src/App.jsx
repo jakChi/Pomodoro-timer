e1819c56eb3724e9aa2337fc4690f72a9318f41b
jakChi <kobachincharauli8@gmail.com>
2024-01-18 14:16:08 +0400

    some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 
+import { useState } from 'react';
 import './App.css'
 
 const App = () => {
-  const hour = "25";
-  const minute = "00";
+  const [brk, setBrk] = useState(5);
+  const [sess, setSess] = useState(25);
+
+  const increment = (param) => {
+    if (param == "break" && brk != 60) {
+      setBrk(brk + 1);
+    } else if (param == "session" && sess != 60) {
+      setSess(sess + 1);
+    }
+  };
+
+  const decrement = (param) => {
+    if (param == "break" && brk != 1) {
+      setBrk(brk - 1);
+    } else if (param == "session" && sess != 1) {
+      setSess(sess - 1);
+    }
+  };
+
+  const reset = () => {
+    setSess(25);
+    setBrk(5);
+  };
 
   return (
     <>
@@ -12,24 +34,32 @@ const App = () => {
         <div id="length-control">
           <div className="length-div">
             <h2 id="break-label">Break Length</h2>
-            <button id="break-decrement">down</button>
-            <h3 id="break-length">5</h3>
-            <button id="break-increment">up</button>
+            <button id="break-decrement" onClick={() => decrement("break")}>
+              down
+            </button>
+            <h3 id="break-length">{brk}</h3>
+            <button id="break-increment" onClick={() => increment("break")}>
+              up
+            </button>
           </div>
           <div className="length-div">
             <h2 id="session-label">Session Length</h2>
-            <button id="session-decrement">down</button>
-            <h3 id="session-length">25</h3>
-            <button id="session-increment">up</button>
+            <button id="session-decrement" onClick={() => decrement("session")}>
+              down
+            </button>
+            <h3 id="session-length">{sess}</h3>
+            <button id="session-increment" onClick={() => increment("session")}>
+              up
+            </button>
           </div>
         </div>
         <div className="timer-box">
           <h2 id="timer-label">Session</h2>
-          <h1 id="time-left">
-            {hour}:{minute}
-          </h1>
+          <div id="time-left">{sess}:00</div>
           <button id="start_stop">play/pouse</button>
-          <button id="reset">reset</button>
+          <button id="reset" onClick={reset}>
+            reset
+          </button>
         </div>
       </div>
     </>
